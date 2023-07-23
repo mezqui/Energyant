9d375f83cdc1cd5da93b8770871d171a6093ceaf
Joaquin Mezquita <joaquinmezquita@hotmail.com>
2023-07-23 18:47:17 -0300

    googleanalytics

--- form.php
+++ form.php
@@ -7,15 +7,18 @@ $mensaje = $_POST['coments'];
 
 $header .=  "Content-Type: text/plain";
 
-$mensaje = "Este mensaje fue enviado por " . $nombre . ".\r\n";
-$mensaje .= "Su email es " . $mail . " \r\n";
-$mensaje .= "Su telefono es " . $telefono . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['coments'] . " \r\n";
+$mensaje = "Este mensaje fue enviado por " . $nombre . "\r\n";
+$mensaje .= "Su email es " . $mail . "\r\n";
+$mensaje .= "Su telefono es " . $telefono . "\r\n";
+$mensaje .= "Mensaje: " . $_POST['coments'] . "\r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
 $para = 'joaquinmezquita@gmail.com';
 $asunto = 'Este mail fue enviado desde Energyant';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
-
-header('Location:index.html');
+if (mail){
+    echo "<h4> ¡Enviado Exitosamente</4>";
+}
+?>
+/* header('Location:index.html'); */
